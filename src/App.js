8969685c6db7a75b364fa19e742f6f39c5b99732
Bhaktij Koli <bhaktijkoli121@gmail.com>
2018-01-14 04:41:24 +0530

    Added Login Form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import {MuiThemeProvider, getMuiTheme} from 'material-ui/styles';
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 
 import Login from './components/Login/Login';
 
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+      <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
           <BrowserRouter>
              <Route exact path="/" component={Login}/>
           </BrowserRouter>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
+import {Paper, FontIcon} from 'material-ui';
+import {Tabs, Tab} from 'material-ui/Tabs';
+import SwipeableViews from 'react-swipeable-views';
 
-class App extends Component {
+import LoginForm from './LoginForm'
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      slideIndex: 0,
+    };
+  }
+
+  handleChange = (value) => {
+    this.setState({
+      slideIndex: value,
+    });
+  };
+
   render() {
     return (
-      <div className="container">
+      <div className="login-content container">
         <div className="row">
-
+          <div className="col-sm-4">
+          </div>
+          <div className="col-sm-4">
+            <Paper>
+              <Tabs
+                onChange={this.handleChange}
+                value={this.state.slideIndex}>
+                <Tab label="Login" value={0} />
+                <Tab label="Sign Up" value={1} />
+              </Tabs>
+              <SwipeableViews
+                index={this.state.slideIndex}
+                onChangeIndex={this.handleChange}>
+                <div className="content">
+                  <LoginForm/>
+                </div>
+                <div className="content">
+                  <h1>Register</h1>
+                </div>
+              </SwipeableViews>
+            </Paper>
+          </div>
         </div>
       </div>
     );
   }
 }
-export default withRouter(App);
+export default withRouter(Login);
