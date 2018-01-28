68ff5fbf3d368dc9e55fd571fb896fa38932da1a
Bhaktij Koli <bhaktijkoli121@gmail.com>
2018-01-28 07:55:01 +0530

    Added gender and dob

--- src/components/Login/RegisterForm.js
+++ src/components/Login/RegisterForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
-import {TextField, RaisedButton } from 'material-ui';
+import {TextField, DatePicker, SelectField, MenuItem, RaisedButton } from 'material-ui';
 
 class RegisterForm extends Component {
   constructor(props) {
@@ -11,16 +11,25 @@ class RegisterForm extends Component {
       lastname:'',
       email:'',
       password:'',
+      gender:'',
+      dob: new Date(),
       firstnameError:'',
       lastnameError:'',
       emailError:'',
       passwordError:'',
+      genderError:"",
     }
   }
   handleChange(e) {
     var name = e.target.name;
     var value = e.target.value;
     this.setState({[name]:value});
+  }
+  handleChangeGender(e, i, v) {
+    this.setState({gender:v})
+  }
+  handleDateChange(e, v) {
+    this.setState({dob:v})
   }
   handleSubmit(e) {
     e.preventDefault();
@@ -31,8 +40,8 @@ class RegisterForm extends Component {
     if(state.lastname.length < 3 || state.firstname.length > 10) this.setState({lastnameError:"Lastname should have of 3-10 characters."}), errors=true;
     if(!this.validateEmail(state.email)) this.setState({emailError:"Email should be valid."}), errors=true;
     if(state.password.length < 4 || state.password.length > 32) this.setState({passwordError:"Password is week.",errors: true}), errors=true;
+    if(state.gender.length < 1) this.setState({genderError:"Select a gender.",errors: true}), errors=true;
     if(errors == true) return;
-    
   }
   render() {
     return (
@@ -41,6 +50,12 @@ class RegisterForm extends Component {
         <TextField floatingLabelText="Last name" fullWidth name="lastname" errorText={this.state.lastnameError} value={this.state.lastname} onChange={e => this.handleChange(e)}/>
         <TextField floatingLabelText="Email" type="email" fullWidth name="email" errorText={this.state.emailError} value={this.state.email} onChange={e => this.handleChange(e)}/>
         <TextField floatingLabelText="Password" type="password" fullWidth name="password" errorText={this.state.passwordError} value={this.state.password} onChange={e => this.handleChange(e)}/>
+        <SelectField floatingLabelText="Gender" fullWidth name="gender" errorText={this.state.genderError} value={this.state.gender} onChange={this.handleChangeGender.bind(this)}>
+          <MenuItem value="Male" primaryText="Male" />
+          <MenuItem value="Female" primaryText="Female" />
+          <MenuItem value="Other" primaryText="Other" />
+        </SelectField>
+        <DatePicker floatingLabelText="Date of birth" fullWidth mode="landscape" name="dob" value={this.state.dob} onChange={this.handleDateChange.bind(this)}/>
         <div className="space"></div>
         <RaisedButton type="submit" label="Sign Up" primary={true} fullWidth/>
         <div className="space"></div>
